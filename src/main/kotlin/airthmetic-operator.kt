00480f249801a4a342d2caf17c393452d58d1fde
junaidiqbalturk <junaidiqbalturk@gmail.com>
2021-02-14 22:17:38 +0500

    Aithmetic Operator in Kotlin

---
+++ src/main/kotlin/airthmetic-operator.kt
@@ -0,0 +1,18 @@
+
+//AIRTHMETIC OPERATORS IN KOTLIN
+
+fun main(){
+    val x = 3 * 4
+    val y = 3 + 4
+    val z = 3 + 4
+    val a = 4 - 3
+    val b = 4 / 3
+    val c = 4 % 2
+    println("The value of x is $x")
+    println("The value of y is $y")
+    println("The value of z is $z")
+    println("The value of a is $a")
+    println("The value of b is $b")
+    println("The value of c is $c")
+
+}
