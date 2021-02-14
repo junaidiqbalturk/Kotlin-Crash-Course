5133f83ea70c7d2e1a946f34044deba9d77d87e9
junaidiqbalturk <junaidiqbalturk@gmail.com>
2021-02-14 22:07:19 +0500

    Variable Declaration Concept

---
+++ src/main/kotlin/variable-concept.kt
@@ -0,0 +1,19 @@
+
+// Kotlin has two ways to declare the variable
+// 1. by using var keyword
+// 2. by usng val keyword
+// the difference between var and val is, val variable cannot be reassigned while var variable can be reassigned.
+
+fun main (){
+    // by Using var keyword
+    var x = 29
+    println("The value of x is $x")
+
+    //by using val keyword
+    val y: Int = 30
+    println("The value of x is $y")
+
+    // We can also declare both var and val like this
+    // var x = 3
+    // val y = 29
+}
